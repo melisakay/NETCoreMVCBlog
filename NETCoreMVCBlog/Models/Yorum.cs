dc84944213c839f4ae90f562fc84fb10db8fbead
Melis Akay <melisbegumakay@yahoo.com>
2023-07-12 20:20:07 +0300

    Propertylere validate eklendi.

--- NETCoreMVCBlog/Models/Yorum.cs
+++ NETCoreMVCBlog/Models/Yorum.cs
@@ -1,9 +1,13 @@
-﻿namespace NETCoreMVCBlog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NETCoreMVCBlog.Models
 {
     public class Yorum
     {
         public int ID { get; set; }
+        [Required]
         public int Yorumİcerik { get; set; }
+        
         public DateTime Tarih = DateTime.Now;
     }
 }
